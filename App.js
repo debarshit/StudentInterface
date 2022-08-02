219a1697e901d53f1240b9753005861ec2f0eaa0
debarshi <dune02deb@gmail.com>
2022-08-02 08:31:06 +0530

    app.js corrected

--- App.js
+++ App.js
@@ -1,4 +1,4 @@
-import { StatusBar as ExpoStatusBar } from "expo-status-bar";';
+import { StatusBar as ExpoStatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from 'react-native';
 import { Searchbar } from "react-native-paper";
 import { StatusBar, StyleSheet, SafeAreaView, Text, View } from "react-native";
