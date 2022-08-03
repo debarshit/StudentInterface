579d506cfb096864b32571041ed673b528134b91
debarshi <dune02deb@gmail.com>
2022-08-03 10:52:34 +0530

    Card component building

--- src/features/teachers/components/teacher-info-card.component.js
+++ src/features/teachers/components/teacher-info-card.component.js
@@ -1,7 +1,10 @@
 import React from "react";
 import styled from "styled-components/native";
-import { Text, StyleSheet } from "react-native";
+import { Text, Image, View } from "react-native";
 import { Card } from "react-native-paper";
+import { SvgXml } from "react-native-svg";
+
+import star from "../../../../assets/star";
 
 const TeacherCard = styled(Card)`
   background-color: ${(props) => props.theme.colors.bg.primary};
@@ -12,29 +15,57 @@ const TeacherCardCover = styled(Card.Cover)`
   background-color: ${(props) => props.theme.colors.bg.primary};
 `;
 
+const Address = styled(Text)`
+  font-family: ${(props) => props.theme.fonts.body};
+  font-size: ${(props) => props.theme.fontSizes.caption};
+`;
+
 const Title = styled(Text)`
-  font-family: ${(props) => props.theme.fonts.body}
-  padding: ${(props) => props.theme.space[3]};
+  font-family: ${(props) => props.theme.fonts.heading};
+  font-size: ${(props) => props.theme.fontSizes.body};
   color: ${(props) => props.theme.colors.ui.primary};
+`;
+
+const Info = styled.View`
+  padding: ${(props) => props.theme.space[3]};
+`;
+
+const Rating = styled.View`
+  flex-direction: row;
+  padding-top: ${(props) => props.theme.space[2]};
+  padding-bottom: ${(props) => props.theme.space[2]};
+`;
+const Section = styled.View`
+  flex-direction: row;
+  align-items: center;
 `;
 
 export const TeacherInfoCard = ({ teacher = {} }) => {
   const {
     name = "Some Teacher",
-    icon,
     photos = [
       "https://www.foodiesfeed.com/wp-content/uploads/2019/06/top-view-for-box-of-2-burgers-home-made-600x899.jpg",
     ],
     address = "100 some random street",
-    isOpenNow = true,
     rating = 4,
-    isClosedTemporarily,
   } = teacher;
+
+  const ratingArray = Array.from(new Array(Math.floor(rating)));
 
   return (
     <TeacherCard elevation={5}>
       <TeacherCardCover key={name} source={{ uri: photos[0] }} />
-      <Title>{name}</Title>
+      <Info>
+        <Title>{name}</Title>
+        <Section>
+          <Rating>
+            {ratingArray.map(() => (
+              <SvgXml xml={star} width={20} height={20} />
+            ))}
+          </Rating>
+        </Section>
+        <Address>{address}</Address>
+      </Info>
     </TeacherCard>
   );
 };
